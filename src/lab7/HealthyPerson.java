8cf2d056782c9b71f94326ca96e75d506c6fdec0
Brian Carlton <brian.carlton@ou.edu>
2019-03-12 15:18:47 -0500

    Initial Upload

---
+++ src/lab7/HealthyPerson.java
@@ -0,0 +1,18 @@
+package lab7;
+
+public class HealthyPerson extends Person {
+
+	public HealthyPerson(String name, int age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+	protected int compareToImpl(Person p)
+	{
+		//TODO
+	}
+	
+	public string toString()
+	{
+		//TODO
+	}
+}
---
+++ src/lab7/SickPerson.java
@@ -0,0 +1,18 @@
+package lab7;
+
+public class SickPerson extends Person {
+	
+	public SickPerson(String name, int age) {
+		super(name, age);
+		// TODO Auto-generated constructor stub
+	}
+	protected int compareToImpl(Person p)
+	{
+		//TODO
+	}
+	
+	public string toString()
+	{
+		//TODO
+	}
+}
